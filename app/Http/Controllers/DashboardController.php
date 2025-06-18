19241325d75a8c346cfd727cc023e7efa2408dd2
Lutfi <mlutfiazizan@gmail.com>
2025-06-19 02:11:37 +0700

    fix dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use App\Models\PurchaseOrder;
-use App\Models\StockItem;
-use App\Models\Supplier;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
+use App\Models\Product;
+use App\Models\Supplier;
+use App\Models\StockItem;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Models\PurchaseOrder;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -36,20 +37,26 @@ class DashboardController extends Controller
             ->with('supplier')->groupBy('supplier_id')->get();
 
         $statusCounts = PurchaseOrder::select('purchase_order_status', DB::raw('COUNT(id) as total'))
-            ->groupBy('purchase_order_status')->get();
+            ->groupBy('purchase_order_status')
+            ->get()
+            ->map(function ($item) {
+                $item->purchase_order_status = Str::ucfirst($item->purchase_order_status);
+                return $item;
+            });
 
         $purchases = PurchaseOrder::select([
             DB::raw("DATE_FORMAT(order_date, '%M') as month"),
             DB::raw('SUM(total_cost) as total'),
             DB::raw('MONTH(order_date) as month_number'), // Add numeric month for sorting
         ])
+            ->where('purchase_order_status', '!=', 'draft')
             ->groupBy('month', 'month_number') // Group by both if needed
             ->orderBy('month_number')
             ->get();
 
         $stockProducts = Product::with('inventory')->get();
 
-        $latestPurchaseOrders = PurchaseOrder::with('Supplier')->where('status', 'active')->orderBy('created_at')->limit(5)->get();
+        $latestPurchaseOrders = PurchaseOrder::with('Supplier')->where('purchase_order_status', '!=', 'received')->where('status', 'active')->orderBy('created_at')->limit(5)->get();
 
         return Inertia::render('dashboard', [
             'totalProducts' => $totalProducts,
